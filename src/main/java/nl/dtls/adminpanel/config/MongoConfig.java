c6216ba2efc75c968edd05e6493ff30b31e272bd
Vojtech Knaisl <vknaisl@gmail.com>
2019-11-12 15:26:26 +0100

    Add Mongo Auth

--- src/main/java/nl/dtls/adminpanel/config/MongoConfig.java
+++ src/main/java/nl/dtls/adminpanel/config/MongoConfig.java
@@ -1,46 +1,18 @@
 package nl.dtls.adminpanel.config;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.gridfs.GridFSBucket;
 import com.mongodb.client.gridfs.GridFSBuckets;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
-@EnableMongoAuditing
-@EnableMongoRepositories(basePackages = {"nl.dtls.adminpanel"})
-public class MongoConfig extends AbstractMongoConfiguration {
-
-    @Value("${spring.data.mongodb.uri}")
-    private String mongoUri;
-
-    @Value("${spring.data.mongodb.database}")
-    private String databaseName;
-
-    @Override
-    protected String getDatabaseName() {
-        return databaseName;
-    }
-
-    @Override
-    public MongoClient mongoClient() {
-        return new MongoClient(new MongoClientURI(mongoUri));
-    }
-
-    @Override
-    protected String getMappingBasePackage() {
-        return "nl.dtls.adminpanel";
-    }
+public class MongoConfig {
 
     @Autowired
     private MappingMongoConverter mongoConverter;
@@ -51,13 +23,14 @@ public class MongoConfig extends AbstractMongoConfiguration {
     }
 
     @Bean
-    public GridFsTemplate gridFsTemplate() throws Exception {
-        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
+    public GridFsTemplate gridFsTemplate(MongoDbFactory mongoDbFactory,
+        MappingMongoConverter mappingMongoConverter) {
+        return new GridFsTemplate(mongoDbFactory, mappingMongoConverter);
     }
 
     @Bean
-    public GridFSBucket getGridFSBuckets() {
-        MongoDatabase db = mongoDbFactory().getDb();
+    public GridFSBucket getGridFSBuckets(MongoDbFactory mongoDbFactory) {
+        MongoDatabase db = mongoDbFactory.getDb();
         return GridFSBuckets.create(db);
     }
 
